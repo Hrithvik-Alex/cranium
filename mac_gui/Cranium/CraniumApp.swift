210ea16996dfea96a3c2af1f4f595071a004071d
halex623 <halex623@gmail.com>
2025-11-06 19:47:22 -0500

    Initial Commit

---
+++ mac_gui/Cranium/CraniumApp.swift
@@ -0,0 +1,17 @@
+//
+//  CraniumApp.swift
+//  Cranium
+//
+//  Created by Hrithvik  Alex on 11/6/25.
+//
+
+import SwiftUI
+
+@main
+struct CraniumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
